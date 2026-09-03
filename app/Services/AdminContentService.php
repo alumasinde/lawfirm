68fe2486f733ce8f0d09e8a17d7ff1d4d3e42df1
Albert Masinde <143938688+alumasinde@users.noreply.github.com>
2026-09-03 21:35:49 +0300

    Import rich text sanitizer in content service

--- app/Services/AdminContentService.php
+++ app/Services/AdminContentService.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Services;
 
 use App\Repositories\AdminContentRepository;
+use App\Support\HtmlSanitizer;
 use InvalidArgumentException;
 
 final class AdminContentService
